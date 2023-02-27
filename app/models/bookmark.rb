c4183b5703be8374585e45126dd66cbbffe13719
LuizGustavo <lgfs2903@gmail.com>
2023-02-27 17:28:12 -0300

    Mycine

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -3,5 +3,5 @@ class Bookmark < ApplicationRecord
   belongs_to :list
 
   validates :comment, length: { minimum: 6 }
-  validates :list, uniqueness: { scope: :movie }
+  validates :movie, uniqueness: { scope: :list }
 end
